2e6eed55ec010504d5af086f99daa326c4c7a78a
slpkid <153894144+slpkid@users.noreply.github.com>
2024-02-23 02:15:34 -0800

    Add function toBlack
    Etch a sketch is functional! On mouseover, the div is now turned black.

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-const container = document.querySelector("#sketch-container")
+const container = document.querySelector("#sketch-container");
 
 function default16x16() {
-    let i = 0
-    while (i < 16) {
+    let i = 0;
+    while (i < 256) {
         const newDiv = document.createElement("div");
-        newDiv.classList.add("box")
+        newDiv.classList.add("box");
         container.appendChild(newDiv);
         i++;
     }
 }
 
 default16x16();
+const boxes = document.querySelectorAll(".box");
+
+function toBlack() {
+    this.style.backgroundColor = "black";
+}
+
+boxes.forEach(div => div.addEventListener('mouseover', toBlack, {
+    capture: false
+}))
